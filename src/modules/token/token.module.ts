3b70f85eb530282cc993228140202f5e84ffc4f3
0xBriz <chaindevone@gmail.com>
2022-12-02 15:10:39 -0500

    setting up token module

--- src/modules/token/token.module.ts
+++ src/modules/token/token.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { CoingeckoService } from './lib/coingecko.service';
+import { TokenPriceService } from './lib/token-price.service';
+import { TokenService } from './token.service';
 
 @Module({
-  providers: [],
-  exports: [],
+  providers: [TokenService, CoingeckoService, TokenPriceService],
+  exports: [TokenService, CoingeckoService, TokenPriceService],
 })
 export class TokenModule {}
