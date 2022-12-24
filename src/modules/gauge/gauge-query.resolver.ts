c4ecb7240d895c3bf18f2bf9138a742871905161
0xBriz <chaindevone@gmail.com>
2022-12-24 18:56:49 -0500

    gauge gql types

--- src/modules/gauge/gauge-query.resolver.ts
+++ src/modules/gauge/gauge-query.resolver.ts
@@ -3,4 +3,9 @@ import { Query, Resolver } from '@nestjs/graphql';
 @Resolver()
 export class GaugeQueryResolver {
   constructor() {}
+
+  @Query()
+  async getLiquidityGauges() {
+    return [];
+  }
 }
