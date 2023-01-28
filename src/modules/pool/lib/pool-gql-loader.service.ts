ceea1af2a0311d114f4a71100dc6f00821830999
0xBriz <chaindevone@gmail.com>
2023-01-28 03:46:14 -0500

    nudder pool id

--- src/modules/pool/lib/pool-gql-loader.service.ts
+++ src/modules/pool/lib/pool-gql-loader.service.ts
@@ -38,6 +38,7 @@ export class PoolGqlLoaderService {
     const remove = [
       '0xf20e2badc2fcd36708f442aa554fd4494e3042c8000200000000000000000004',
       '0x94cbf85d049ab5a122348324b53dc4a9b751f08400020000000000000000000d',
+      '0xeb49e7e8ebdd44651c53eca40a844866d8825ab3000200000000000000000005',
     ];
     pools.forEach((p) => {
       if (!remove.find((pi) => pi === p.id)) {
