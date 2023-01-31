3eeecbae5f0d0475b6a8e2ad0bd325366b3cfbfe
0xBriz <chaindevone@gmail.com>
2023-01-31 13:51:25 -0500

    added error check for balance sync

--- src/modules/user/lib/user-sync-gauge-balance.service.ts
+++ src/modules/user/lib/user-sync-gauge-balance.service.ts
@@ -72,134 +72,151 @@ export class UserSyncGaugeBalanceService implements UserStakedBalanceService {
   }
 
   async syncChangedStakedBalances(): Promise<void> {
-    // we always store the latest synced block
-    const status = await this.prisma.prismaUserBalanceSyncStatus.findUnique({
-      where: { type: 'STAKED' },
-    });
+    try {
+      // we always store the latest synced block
+      const status = await this.prisma.prismaUserBalanceSyncStatus.findUnique({
+        where: { type: 'STAKED' },
+      });
 
-    if (!status) {
-      throw new Error(
-        'UserSyncGaugeBalanceService: syncStakedBalances called before initStakedBalances',
-      );
-    }
+      if (!status) {
+        throw new Error(
+          'UserSyncGaugeBalanceService: syncStakedBalances called before initStakedBalances',
+        );
+      }
 
-    const jsonRpcProvider = this.rpc.provider;
-    const pools = await this.prisma.prismaPool.findMany({ include: { staking: true } });
-    const latestBlock = await jsonRpcProvider.getBlockNumber();
-    const gaugeAddresses = await this.gaugeService.getAllGaugeAddresses();
+      const jsonRpcProvider = this.rpc.provider;
+      const pools = await this.prisma.prismaPool.findMany({ include: { staking: true } });
+      const latestBlock = await jsonRpcProvider.getBlockNumber();
+      const gaugeAddresses = await this.gaugeService.getAllGaugeAddresses();
 
-    // we sync at most 10k blocks at a time
-    const startBlock = status.blockNumber + 1;
-    const endBlock = latestBlock - startBlock > 2_000 ? startBlock + 2_000 : latestBlock;
+      // we sync at most 10k blocks at a time
+      const startBlock = status.blockNumber + 1;
+      const endBlock = latestBlock - startBlock > 2_000 ? startBlock + 2_000 : latestBlock;
 
-    const multicall = new Multicaller(this.rpc, LiqGaugeV5abi);
+      const multicall = new Multicaller(this.rpc, LiqGaugeV5abi);
 
-    // the multicall response will be merged into this object
-    let response: {
-      [gauge: string]: { [userAddress: string]: BigNumber };
-    } = {};
+      // the multicall response will be merged into this object
+      let response: {
+        [gauge: string]: { [userAddress: string]: BigNumber };
+      } = {};
 
-    // we keep track of all user addresses to create them as entities in the db
-    const allUserAddress: string[] = [];
+      // we keep track of all user addresses to create them as entities in the db
+      const allUserAddress: string[] = [];
 
-    /*
+      /*
             we need to figure out which users have a changed balance on any gauge contract and update their balance,
             therefore we check all deposit, withdraw and transfer events since the last synced block
          */
-    for (let gaugeAddress of gaugeAddresses) {
-      const gauge = this.contractService.getLiquidityGauge(gaugeAddress);
+      for (let gaugeAddress of gaugeAddresses) {
+        const gauge = this.contractService.getLiquidityGauge(gaugeAddress);
 
-      // so we get all events since the last synced block
-      const events = await gauge.queryFilter({ address: gaugeAddress }, startBlock, endBlock);
+        // so we get all events since the last synced block
+        const events = await gauge.queryFilter({ address: gaugeAddress }, startBlock, endBlock);
 
-      // we filter by those events and avoid duplicated users per gauge contract by utlizing a Set
-      const uniqueUserAddresses = new Set<string>();
-      const filteredEvents = events.filter((event) =>
-        ['Deposit', 'Withdraw', 'Transfer'].includes(event.event!),
-      );
+        // we filter by those events and avoid duplicated users per gauge contract by utlizing a Set
+        const uniqueUserAddresses = new Set<string>();
+        const filteredEvents = events.filter((event) =>
+          ['Deposit', 'Withdraw', 'Transfer'].includes(event.event!),
+        );
 
-      for (let event of filteredEvents) {
-        if (event.event === 'Transfer') {
-          if (event.args!._from !== ZERO_ADDRESS && event.args!._to !== ZERO_ADDRESS) {
-            uniqueUserAddresses.add(event.args!._from.toLowerCase());
-            uniqueUserAddresses.add(event.args!._to.toLowerCase());
+        for (let event of filteredEvents) {
+          if (event.event === 'Transfer') {
+            if (event.args!._from !== ZERO_ADDRESS && event.args!._to !== ZERO_ADDRESS) {
+              uniqueUserAddresses.add(event.args!._from.toLowerCase());
+              uniqueUserAddresses.add(event.args!._to.toLowerCase());
+            }
+          } else {
+            uniqueUserAddresses.add(event.args!.provider.toLowerCase());
           }
-        } else {
-          uniqueUserAddresses.add(event.args!.provider.toLowerCase());
         }
+
+        for (const userAddress of uniqueUserAddresses) {
+          // a dot in the path nests the response on this key
+          multicall.call(`${gaugeAddress}.${userAddress}`, gaugeAddress, 'balanceOf', [
+            userAddress,
+          ]);
+
+          // so if we scheduled more than 100 calls, we execute the batch
+          if (multicall.numCalls >= 100) {
+            response = _.merge(response, await multicall.execute());
+          }
+        }
+        allUserAddress.push(...uniqueUserAddresses);
+      }
+      // see if we have some more calls to execute
+      if (multicall.numCalls > 0) {
+        response = _.merge(response, await multicall.execute());
       }
 
-      for (const userAddress of uniqueUserAddresses) {
-        // a dot in the path nests the response on this key
-        multicall.call(`${gaugeAddress}.${userAddress}`, gaugeAddress, 'balanceOf', [userAddress]);
-
-        // so if we scheduled more than 100 calls, we execute the batch
-        if (multicall.numCalls >= 100) {
-          response = _.merge(response, await multicall.execute());
-        }
-      }
-      allUserAddress.push(...uniqueUserAddresses);
-    }
-    // see if we have some more calls to execute
-    if (multicall.numCalls > 0) {
-      response = _.merge(response, await multicall.execute());
-    }
-
-    /*
+      /*
             we have an object with gaugeAddress => userAddress => balance,
             the 2nd argument of the lodash _.map function provides the key of the object
          */
-    const userGaugeBalanceUpdates = _.map(response, (userBalance, gaugeAddress) => {
-      // now we have an object userAddress => balance, so 2nd argument is the key which is the user address
-      return _.map(userBalance, (amount, userAddress) => ({
-        gaugeAddress,
-        userAddress: userAddress.toLowerCase(),
-        amount: formatFixed(amount, 18),
-      }));
-    }).flat();
+      const userGaugeBalanceUpdates = _.map(response, (userBalance, gaugeAddress) => {
+        // now we have an object userAddress => balance, so 2nd argument is the key which is the user address
+        return _.map(userBalance, (amount, userAddress) => ({
+          gaugeAddress,
+          userAddress: userAddress.toLowerCase(),
+          amount: formatFixed(amount, 18),
+        }));
+      }).flat();
 
-    if (userGaugeBalanceUpdates.length === 0) {
-      await this.prisma.prismaUserBalanceSyncStatus.update({
-        where: { type: 'STAKED' },
-        data: { blockNumber: endBlock },
-      });
-
-      return;
-    }
-
-    await prismaBulkExecuteOperations(
-      [
-        this.prisma.prismaUser.createMany({
-          data: _.uniq(allUserAddress).map((address) => ({ address })),
-          skipDuplicates: true,
-        }),
-        ...userGaugeBalanceUpdates.map((update) => {
-          const pool = pools.find((pool) => pool.staking?.id === update.gaugeAddress);
-
-          return this.prisma.prismaUserStakedBalance.upsert({
-            where: { id: `${update.gaugeAddress}-${update.userAddress}` },
-            update: {
-              balance: update.amount,
-              balanceNum: parseFloat(update.amount),
-            },
-            create: {
-              id: `${update.gaugeAddress}-${update.userAddress}`,
-              balance: update.amount,
-              balanceNum: parseFloat(update.amount),
-              userAddress: update.userAddress,
-              poolId: pool?.id,
-              tokenAddress: pool?.address,
-              stakingId: update.gaugeAddress,
-            },
-          });
-        }),
-        this.prisma.prismaUserBalanceSyncStatus.update({
+      if (userGaugeBalanceUpdates.length === 0) {
+        await this.prisma.prismaUserBalanceSyncStatus.update({
           where: { type: 'STAKED' },
           data: { blockNumber: endBlock },
-        }),
-      ],
-      true,
-    );
+        });
+
+        return;
+      }
+
+      await prismaBulkExecuteOperations(
+        [
+          this.prisma.prismaUser.createMany({
+            data: _.uniq(allUserAddress).map((address) => ({ address })),
+            skipDuplicates: true,
+          }),
+
+          ...userGaugeBalanceUpdates
+            .map((update) => {
+              const pool = pools.find((pool) => pool.staking?.id === update.gaugeAddress);
+
+              console.log(pool);
+              if (!pool?.id) {
+                console.log(pool);
+                console.log('NO POOL???');
+                return;
+              }
+
+              return this.prisma.prismaUserStakedBalance.upsert({
+                where: { id: `${update.gaugeAddress}-${update.userAddress}` },
+                update: {
+                  balance: update.amount,
+                  balanceNum: parseFloat(update.amount),
+                },
+                create: {
+                  id: `${update.gaugeAddress}-${update.userAddress}`,
+                  balance: update.amount,
+                  balanceNum: parseFloat(update.amount),
+                  userAddress: update.userAddress,
+                  poolId: pool?.id,
+                  tokenAddress: pool?.address,
+                  stakingId: update.gaugeAddress,
+                },
+              });
+            })
+            .filter((op) => op !== undefined),
+
+          this.prisma.prismaUserBalanceSyncStatus.update({
+            where: { type: 'STAKED' },
+            data: { blockNumber: endBlock },
+          }),
+        ],
+        true,
+      );
+    } catch (error) {
+      console.error('Error syncing gauge balances', error);
+    }
   }
 
   async syncUserBalance({ userAddress, poolId, poolAddress, staking }: UserSyncUserBalanceInput) {
