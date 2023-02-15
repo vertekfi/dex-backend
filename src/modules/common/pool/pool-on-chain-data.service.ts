7214c76ed4f4a9b695a034d9a6907bc30427fe8c
0xBriz <chaindevone@gmail.com>
2023-02-15 10:17:42 -0500

    added multicall id

--- src/modules/common/pool/pool-on-chain-data.service.ts
+++ src/modules/common/pool/pool-on-chain-data.service.ts
@@ -140,7 +140,9 @@ export class PoolOnChainDataService {
     let poolsOnChainData = {} as Record<string, MulticallExecuteResult>;
 
     try {
-      poolsOnChainData = (await multiPool.execute()) as Record<string, MulticallExecuteResult>;
+      poolsOnChainData = (await multiPool.execute(
+        'PoolOnChainDataService:updateOnChainDataV1',
+      )) as Record<string, MulticallExecuteResult>;
     } catch (err: any) {
       console.error(err);
       throw `Issue with multicall execution. ${err}`;
